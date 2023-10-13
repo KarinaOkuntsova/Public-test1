5f585e8b561684fecc5e6d6e08b4cc8f467c4c00
KarinaOkuntsova <124935730+KarinaOkuntsova@users.noreply.github.com>
2023-10-13 23:25:19 +0300

    Update Class_Cat.py

--- Class_Cat.py
+++ Class_Cat.py
@@ -1,14 +1,14 @@
 class Cat
    def __init__(self, name, gender, age):
        self.name = name
-	   self.gender = gender
-	   self.age = age
+       self.gender = gender
+       self.age = age
 	   
-	def getName(self):
-	    return self.name
+   def getName(self):
+       return self.name
 		
-	def getGender(self):
-	    return self.gender
+   def getGender(self):
+       return self.gender
 		
-	der getAge(self):
-	    return self.age
+   der getAge(self):
+       return self.age
