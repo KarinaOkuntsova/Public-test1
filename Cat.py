c43fb6f3040f545e2f8fd8f62da4b25718d16fe2
KarinaOkuntsova <124935730+KarinaOkuntsova@users.noreply.github.com>
2023-10-13 23:32:45 +0300

    Update Cat.py

--- Cat.py
+++ Cat.py
@@ -1,1 +1,13 @@
+from Cat_Class import Cat
 
+Cat1 = Cat("Baron", "male", 2)
+
+print("Cat1.name=", Cat1.name)
+print("Cat1.gender=", Cat1.gender)
+print("Cat1.age=", Cat1.age)
+
+Cat2 = Cat("Sam", "male", 2)
+
+print("Cat2.name=", Cat2.name)
+print("Cat2.gender=", Cat2.gender)
+print("Cat2.age=", Cat2.age)
