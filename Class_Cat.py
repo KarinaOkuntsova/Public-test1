4f6028dadff5a5f1e8e4da573a1999d9b37f9b0d
KarinaOkuntsova <124935730+KarinaOkuntsova@users.noreply.github.com>
2023-10-13 23:17:57 +0300

    Create Class_Cat.py

---
+++ Class_Cat.py
@@ -0,0 +1,14 @@
+class Cat
+   def __init__(self, name, gender, age):
+       self.name = name
+	   self.gender = gender
+	   self.age = age
+	   
+	def getName(self):
+	    return self.name
+		
+	def getGender(self):
+	    return self.gender
+		
+	der getAge(self):
+	    return self.age
